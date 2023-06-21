90400730ea3248b34724c0cc145c73a2012aac15
RomanLandwehr <roman.landwehr@stud.uni-hannover.de>
2023-06-21 11:52:12 +0200

    Object.py

--- Grafic.py
+++ Grafic.py
@@ -2,20 +2,32 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 
+#####Lege die Schrittweite fest
+step = 1000
+#####Lege die Leistung fest, bis zu der die NAPs betrachtet werden sollen
+maximum = 1000000
+
+#####Lese das Dataframe ein. Die Datei AuslastungNAPs.xlsx wir durch 
+#####Ausführung von main.py erstellt
 df = pd.read_excel('AuslastungNAPs.xlsx')
 df = df.set_index('MastrNummer')
+#####Lösche alle unvollständingen (Nettoengpassleistung = 0) NAPs
 df = df.drop(df[df['Nettoengpassleistung'] == 0].index)
+#####Betrachte nur NAps mit Auslastung > 1
+df = df.drop(df[df['Auslastung'] <= 1].index)
 df = df[~df.index.duplicated(keep='first')]
 
 
 y = 0
 Key = []
 Count = []
+iterations = int(maximum/step)
 
-for i in tqdm(range(20)):
+#####Zähle die entsprechenden NAPs gemaess Gruppierung
+for i in tqdm(range(iterations)):
     count = 0
     x = y
-    y = x + 500
+    y = x + step
     for MastrNummer in df.index:
         Nettoengpassleistung = df.loc[MastrNummer]['Nettoengpassleistung']
         if Nettoengpassleistung > x:
@@ -27,11 +39,11 @@ for i in tqdm(range(20)):
         Key.append(key)
         Count.append(count)
 
+#####Erstelle DataFrame
 df2 = pd.DataFrame([Key,Count]).transpose()  
-df2.columns= ['Nettoengpassleistung','Anzahl']
-#df2 = df2.set_index(0)    
+df2.columns= ['Netzanschlusskapazität','Anzahl']
 
-
+#####Erstelle Diagramme
 ax = df2.plot.bar(x=0,y='Anzahl')
 ax.set_xticks(df2.index[::2])
 plt.show()
---
+++ Object.py
@@ -0,0 +1,4 @@
+class Object:
+    def __init__(self, Name, Tag):
+        self.Name = Name
+        self.Tag = Tag
